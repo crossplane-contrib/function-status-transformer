9270005f13b2e2524896234f59a75fe884b6b9a0
dalton hill <dalton.hill.0@protonmail.com>
2024-08-15 17:22:14 -0500

    Improving the API to add additional functionality and allow for more concise usage.
    Signed-off-by: dalton hill <dalton.hill.0@protonmail.com>

--- fn.go
+++ fn.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"regexp"
 	"text/template"
 
@@ -19,6 +20,8 @@ import (
 	"github.com/crossplane/function-status-transformer/input/v1beta1"
 )
 
+type contextKey string
+
 const (
 	// Condition types.
 	typeFunctionSuccess = "StatusTransformationSuccess"
@@ -29,6 +32,9 @@ const (
 	reasonObservedCompositeFailure = "ObservedCompositeFailure"
 	reasonMatchFailure             = "MatchFailure"
 	reasonSetConditionFailure      = "SetConditionFailure"
+
+	// Context keys.
+	logKey contextKey = "log"
 )
 
 // Function returns whatever response you ask it to.
@@ -39,7 +45,7 @@ type Function struct {
 }
 
 // RunFunction runs the Function.
-func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
+func (f *Function) RunFunction(ctx context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
 	log := f.log.WithValues("tag", req.GetMeta().GetTag())
 	log.Debug("running function")
 
@@ -47,19 +53,19 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 
 	in := &v1beta1.StatusTransformation{}
 	if err := request.GetInput(req, in); err != nil {
-		msg := errors.Wrapf(err, "cannot get Function input from %T", req).Error()
-		log.Info(msg)
+		msg := fmt.Sprintf("cannot get Function input from %T", req)
+		log.Info(msg, "error", err)
 		response.ConditionFalse(rsp, typeFunctionSuccess, reasonInputFailure).
-			WithMessage(msg)
+			WithMessage(errors.Wrap(err, msg).Error())
 		return rsp, nil
 	}
 
 	xr, err := request.GetObservedCompositeResource(req)
 	if err != nil {
-		msg := errors.Wrapf(err, "cannot get observed XR from %T", req).Error()
-		log.Info(msg)
+		msg := fmt.Sprintf("cannot get observed XR from %T", req)
+		log.Info(msg, "error", err)
 		response.ConditionFalse(rsp, typeFunctionSuccess, reasonInputFailure).
-			WithMessage(msg)
+			WithMessage(errors.Wrap(err, msg).Error())
 		return rsp, nil
 	}
 	log = log.WithValues(
@@ -77,46 +83,56 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 	errored := false
 	conditionsSet := map[string]bool{}
 	for shi, sh := range in.StatusConditionHooks {
+		log := log.WithValues("statusConditionHookIndex", shi)
 		// The regular expression groups found in the matches.
 		scGroups := map[string]string{}
-		shMatched := true
-		for mci, mc := range sh.MatchConditions {
-			mcMatched, mcGroups, err := matchConditions(mc, observed)
+		allMatched := false
+		for mci, mc := range sh.Matchers {
+			log := log.WithValues("matchConditionIndex", mci)
+			ctx := context.WithValue(ctx, logKey, log)
+
+			matched, mcGroups, err := matchResources(ctx, mc, observed)
 			if err != nil {
-				log.Info("error when matching", "error", err, "statusConditionHookIndex", shi, "matchConditionIndex", mci)
+				log.Info("cannot match resources", "error", err)
 				response.ConditionFalse(rsp, typeFunctionSuccess, reasonMatchFailure).
-					WithMessage(errors.Wrapf(err, "error when matching, statusConditionHookIndex: %d, matchConditionIndex: %d", shi, mci).Error())
-				mcMatched = false
+					WithMessage(errors.Wrapf(err, "cannot match resources, statusConditionHookIndex: %d, matchConditionIndex: %d", shi, mci).Error())
+				matched = false
 				errored = true
 			}
-			if !mcMatched {
-				shMatched = false
+
+			if !matched {
+				// All matchConditions must match.
+				allMatched = false
 				break
 			}
+			allMatched = true
+
 			// All matches were successful, copy over any regex groups.
 			for k, v := range mcGroups {
 				scGroups[k] = v
 			}
 		}
 
-		if !shMatched {
+		if !allMatched {
 			// This hook did not match; do not set conditions.
 			continue
 		}
 
 		// All matchConditions matched, set the desired conditions.
 		for sci, cs := range sh.SetConditions {
+			log := log.WithValues("setConditionIndex", sci)
 			if conditionsSet[cs.Condition.Type] && (cs.Force == nil || !*cs.Force) {
 				// The condition is already set and this setter is not forceful.
+				log.Debug("skipping because condition is already set and setCondition is not forceful")
 				continue
 			}
+			log.Debug("setting condition")
 
 			c, err := transformCondition(cs, scGroups)
 			if err != nil {
-				msg := errors.Wrapf(err, "failed to set condition, statusConditionHookIndex: %d, setConditionIndex: %d", shi, sci).Error()
-				log.Info(msg)
+				log.Info("cannot set condition", "error", err)
 				response.ConditionFalse(rsp, typeFunctionSuccess, reasonSetConditionFailure).
-					WithMessage(msg)
+					WithMessage(errors.Wrapf(err, "cannot set condition, statusConditionHookIndex: %d, setConditionIndex: %d", shi, sci).Error())
 				errored = true
 				continue
 			}
@@ -126,12 +142,12 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 		}
 
 		for cei, ce := range sh.CreateEvents {
+			log := log.WithValues("createEventIndex", cei)
 			r, err := transformEvent(ce, scGroups)
 			if err != nil {
-				msg := errors.Wrapf(err, "failed to create event, statusConditionHookIndex: %d, createEventIndex: %d", shi, cei).Error()
-				log.Info(msg)
+				log.Info("cannot create event")
 				response.ConditionFalse(rsp, typeFunctionSuccess, reasonSetConditionFailure).
-					WithMessage(msg)
+					WithMessage(errors.Wrapf(err, "cannot create event, statusConditionHookIndex: %d, createEventIndex: %d", shi, cei).Error())
 				errored = true
 				continue
 			}
@@ -147,66 +163,190 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 	return rsp, nil
 }
 
-func matchConditions(cm v1beta1.MatchCondition, om map[string]*fnv1beta1.Resource) (bool, map[string]string, error) {
-	re, err := regexp.Compile(cm.ResourceName)
-	if err != nil {
-		return false, nil, errors.Join(errors.New("failed to compile resourceName regex"), err)
-	}
-	cmGroups := map[string]string{}
-	matchedAny := false
-	matchedAll := true
-	for k, o := range om {
-		if !re.MatchString(k) {
-			continue
-		}
-		// for each observed object with a resource name matching the regex,
-		// check if the status condition matches the match condition
-		conditioned := xpv1.ConditionedStatus{}
-		// Ignoring error. If field is missing, we will default to unknown.
-		_ = fieldpath.Pave(o.GetResource().AsMap()).GetValueInto("status", &conditioned)
-		c := conditioned.GetCondition(xpv1.ConditionType(cm.Condition.Type))
-		if cm.Condition.Reason != nil && *cm.Condition.Reason != string(c.Reason) {
-			matchedAll = false
-			continue
-		}
+func matchResources(ctx context.Context, mc v1beta1.Matcher, observedMap map[string]*fnv1beta1.Resource) (bool, map[string]string, error) {
+	log := ctx.Value(logKey).(logging.Logger)
 
-		if cm.Condition.Status != nil && *cm.Condition.Status != v1.ConditionStatus(c.Status) {
-			matchedAll = false
-			continue
-		}
-
-		if cm.Condition.Message == nil {
-			matchedAny = true
-			continue
-		}
-
-		// match message and build up map of args
-		re, err := regexp.Compile(*cm.Condition.Message)
+	rs := map[string]*fnv1beta1.Resource{}
+	for i, r := range mc.Resources {
+		re, err := regexp.Compile(r.Key)
 		if err != nil {
-			return false, nil, errors.Join(errors.New("failed to compile message regex"), err)
+			log.Info("cannot compile resource key regex", "resourcesIndex", i, "error", err)
+			return false, nil, errors.Wrapf(err, "cannot compile resource key regex, resourcesIndex: %d", i)
 		}
-
-		matches := re.FindStringSubmatch(c.Message)
-		if len(matches) == 0 {
-			matchedAll = false
-			continue
-		}
-		matchedAny = true
-
-		for i := 1; i < len(matches); i++ {
-			cmGroups[re.SubexpNames()[i]] = matches[i]
+		for k, v := range observedMap {
+			if re.MatchString(k) {
+				rs[k] = v
+			}
 		}
 	}
 
-	var matched bool
-	switch ptr.Deref(cm.Type, v1beta1.MatchAll) {
-	case v1beta1.MatchAll:
-		matched = matchedAll
-	case v1beta1.MatchAny:
-		matched = matchedAny
+	if len(rs) == 0 {
+		// There are no resources to match against.
+		return false, nil, nil
+	}
+	if len(mc.Conditions) == 0 {
+		// There are no conditions to match against.
+		return false, nil, nil
 	}
 
-	return matched, cmGroups, nil
+	switch ptr.Deref(mc.Type, v1beta1.AllResourcesMatchAllConditions) {
+	case v1beta1.AnyResourceMatchesAnyCondition:
+		return anyResourceMatchesAnyCondition(ctx, mc.Conditions, rs)
+	case v1beta1.AnyResourceMatchesAllConditions:
+		return anyResourceMatchesAllConditions(ctx, mc.Conditions, rs)
+	case v1beta1.AllResourcesMatchAnyCondition:
+		return allResourcesMatchAnyConditions(ctx, mc.Conditions, rs)
+	case v1beta1.AllResourcesMatchAllConditions:
+		fallthrough
+	default:
+		return allResourcesMatchAllConditions(ctx, mc.Conditions, rs)
+	}
+}
+
+func anyResourceMatchesAnyCondition(ctx context.Context, cms []v1beta1.ConditionMatcher, rm map[string]*fnv1beta1.Resource) (bool, map[string]string, error) {
+	log := ctx.Value(logKey).(logging.Logger)
+	for k, r := range rm {
+		for cmi, cm := range cms {
+			log := log.WithValues("resource", k, "conditionIndex", cmi)
+			ctx := context.WithValue(ctx, logKey, log)
+			m, cg, err := match(ctx, cm, r)
+			if err != nil {
+				log.Info("cannot match resource", "error", err)
+				return false, nil, err
+			}
+
+			if m {
+				return true, cg, nil
+			}
+		}
+	}
+
+	return false, nil, nil
+}
+
+func anyResourceMatchesAllConditions(ctx context.Context, cms []v1beta1.ConditionMatcher, rm map[string]*fnv1beta1.Resource) (bool, map[string]string, error) {
+	log := ctx.Value(logKey).(logging.Logger)
+	capturedGroups := map[string]string{}
+	for k, r := range rm {
+		matched := 0
+		for cmi, cm := range cms {
+			log := log.WithValues("resource", k, "conditionIndex", cmi)
+			ctx := context.WithValue(ctx, logKey, log)
+			m, cg, err := match(ctx, cm, r)
+			if err != nil {
+				log.Info("cannot match resource", "error", err)
+				return false, nil, err
+			}
+			if !m {
+				break
+			}
+			matched++
+			for k, v := range cg {
+				capturedGroups[k] = v
+			}
+		}
+		if matched == len(cms) {
+			return true, capturedGroups, nil
+		}
+	}
+
+	return false, nil, nil
+}
+
+func allResourcesMatchAnyConditions(ctx context.Context, cms []v1beta1.ConditionMatcher, rm map[string]*fnv1beta1.Resource) (bool, map[string]string, error) {
+	log := ctx.Value(logKey).(logging.Logger)
+	capturedGroups := map[string]string{}
+	for k, r := range rm {
+		matched := 0
+		for cmi, cm := range cms {
+			log := log.WithValues("resource", k, "conditionIndex", cmi)
+			ctx := context.WithValue(ctx, logKey, log)
+			m, cg, err := match(ctx, cm, r)
+			if err != nil {
+				log.Info("cannot match resource", "error", err)
+				return false, nil, err
+			}
+			if !m {
+				continue
+			}
+			matched++
+			for k, v := range cg {
+				capturedGroups[k] = v
+			}
+		}
+		if matched == 0 {
+			return false, nil, nil
+		}
+	}
+
+	return true, capturedGroups, nil
+}
+
+func allResourcesMatchAllConditions(ctx context.Context, cms []v1beta1.ConditionMatcher, rm map[string]*fnv1beta1.Resource) (bool, map[string]string, error) {
+	log := ctx.Value(logKey).(logging.Logger)
+	capturedGroups := map[string]string{}
+	for k, r := range rm {
+		for cmi, cm := range cms {
+			log := log.WithValues("resource", k, "conditionIndex", cmi)
+			ctx := context.WithValue(ctx, logKey, log)
+			m, cg, err := match(ctx, cm, r)
+			if err != nil {
+				log.Info("cannot match resource", "error", err)
+				return false, nil, err
+			}
+			if !m {
+				return false, nil, nil
+			}
+			for k, v := range cg {
+				capturedGroups[k] = v
+			}
+		}
+	}
+
+	return true, capturedGroups, nil
+}
+
+func match(ctx context.Context, cm v1beta1.ConditionMatcher, r *fnv1beta1.Resource) (bool, map[string]string, error) {
+	log := ctx.Value(logKey).(logging.Logger)
+	cmGroups := map[string]string{}
+	conditioned := xpv1.ConditionedStatus{}
+
+	// Ignoring error. If field is missing, we will default to unknown.
+	_ = fieldpath.Pave(r.GetResource().AsMap()).GetValueInto("status", &conditioned)
+	c := conditioned.GetCondition(xpv1.ConditionType(cm.Type))
+	if cm.Reason != nil && *cm.Reason != string(c.Reason) {
+		log.Debug(fmt.Sprintf("condition reason \"%s\" did not match \"%s\"", c.Reason, *cm.Reason))
+		return false, nil, nil
+	}
+
+	if cm.Status != nil && *cm.Status != v1.ConditionStatus(c.Status) {
+		log.Debug(fmt.Sprintf("condition status \"%s\" did not match \"%s\"", c.Status, *cm.Status))
+		return false, nil, nil
+	}
+
+	if cm.Message == nil {
+		log.Debug("condition matched")
+		return true, nil, nil
+	}
+
+	// match message and build up map of args
+	re, err := regexp.Compile(*cm.Message)
+	if err != nil {
+		return false, nil, errors.Wrap(err, "cannot compile message regex")
+	}
+
+	matches := re.FindStringSubmatch(c.Message)
+	if len(matches) == 0 {
+		log.Debug(fmt.Sprintf("condition message \"%s\" did not match \"%s\"", c.Message, *cm.Message))
+		return false, nil, nil
+	}
+
+	for i := 1; i < len(matches); i++ {
+		cmGroups[re.SubexpNames()[i]] = matches[i]
+	}
+	log.Debug(fmt.Sprintf("condition matched - total captured groups: %v", cmGroups))
+
+	return true, cmGroups, nil
 }
 
 func transformCondition(cs v1beta1.SetCondition, templateValues map[string]string) (*fnv1beta1.Condition, error) {
@@ -274,11 +414,11 @@ func templateMessage(msg *string, values map[string]string) (*string, error) {
 
 	t, err := template.New("").Parse(*msg)
 	if err != nil {
-		return nil, errors.Join(errors.New("failed to parse template"), err)
+		return nil, errors.Wrap(err, "cannot parse template")
 	}
 	b := bytes.NewBuffer(nil)
 	if err := t.Execute(b, values); err != nil {
-		return nil, errors.Join(errors.New("failed to execute template"), err)
+		return nil, errors.Wrap(err, "cannot execute template")
 	}
 	return ptr.To(b.String()), nil
 }
